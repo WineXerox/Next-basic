a59a3687ee6ff392192526dd7be7a2b904c25920
ธนพล ธนะศรี <tanapol.ta.61@ubu.ac.th>
2024-01-16 04:50:37 +0700

    Add Project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+      },
+    ],
+    unoptimized:true
+  },
 }
 
 module.exports = nextConfig
